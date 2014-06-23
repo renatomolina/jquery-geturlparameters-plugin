59247fae17d85b712069cb224d1ac81ce6ae7954
Renato Molina <renato.molina@inspira.com.br>
2014-06-23 19:25:05 -0300

    Add plugin to get url parameters from GET method

---
+++ jquery.getUrlParameters.js
@@ -0,0 +1,6 @@
+(function($){
+	$.getUrlVar = function(key){
+		var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search); 
+		return result && unescape(result[1]) || ""; 
+	};
+})(jQuery);
